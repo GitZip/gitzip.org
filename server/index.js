90f18f48aaab3e6d9223a45696c243d6aa7dc041
KinoLien <miecowbai@gmail.com>
2017-09-04 14:09:57 +0800

    Force HTTPS in Production.

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ require('./config/express')(app);
 // routes
 require('./config/routes')(app);
 
-http.createServer(app).listen(80);
-
 if(isProduction){
 	var https = require('https');
 	var fs = require('fs');
@@ -24,5 +22,6 @@ if(isProduction){
 	};
 
 	https.createServer(options, app).listen(443);
+}else{
+	http.createServer(app).listen(80);
 }
-
